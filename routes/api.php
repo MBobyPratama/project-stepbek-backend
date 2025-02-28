cfbc49fbab772346d515776278b1b58f2e421828
Muhammad Boby Pratama <bobyjakarta4@gmail.com>
2025-02-28 14:46:43 +0700

    feat(event): add event & get all event)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,5 +13,18 @@ Route::post('register', [AuthController::class, 'register']);
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
+});
+
+// User routes
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('users', [UserController::class, 'index']);
+});
+
+// Event routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('events', [EventController::class, 'index']);
+    Route::post('events', [EventController::class, 'store']);
+    Route::get('events/{id}', [EventController::class, 'show']);
+    Route::put('events/{id}', [EventController::class, 'update']);
+    Route::delete('events/{id}', [EventController::class, 'destroy']);
 });
