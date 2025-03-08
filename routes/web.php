d5d5c631b2dde1ce29b9e7d0c56414b716a514e2
Muhammad Boby Pratama <bobyjakarta4@gmail.com>
2025-03-08 20:41:26 +0700

    fixx jag

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    Artisan::call('storage:link');
 });
